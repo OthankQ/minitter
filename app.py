f225d1bc09f3e45e5d6bc8c0d1e1c9672cc11aa0
Ryan <kim1885@purdue.edu>
2019-05-21 21:57:00 +0900

    add follow endpoint and tweet endpoint

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 app.id_count = 1
 app.users = {}
+app.tweets = []
 
 
 @app.route("/ping", methods=['GET'])
 def ping():
     return "Pong"
 
-
+# Sign-up
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
     new_user = request.json
@@ -18,3 +19,45 @@ def sign_up():
     app.id_count += 1
 
     return jsonify(new_user)
+
+# 300 Letter limited tweet
+@app.route("/tweet", methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet = payload['tweet']
+
+    if user_id not in app.users:
+        return 'Non existent user', 400
+
+    if len(tweet) > 300:
+        return 'Tweet is too long', 400
+
+    app.tweets.append({
+        'user_id': user_id,
+        'tweet': tweet
+    })
+
+    return 'Your tweet has been submitted', 200
+
+# follow other users
+@app.route("/follow", methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload["id"])
+    user_to_follow = int(payload["follow"])
+
+    if user_id not in app.users or user_to_follow not in app.users:
+        return 'User with that id does not exist', 400
+
+    user = app.users[user_id]
+    user.setdefault('follow', set()).add(user_to_follow)
+
+    return jsonify(user)
+
+# unfollow other users
+@app.route("/unfollow", methods=['POST'])
+def unfollow():
+    payload = request.json
+    user_id = int(payload["id"])
+    user_to_unfollow = int(payload["unfollow"])
